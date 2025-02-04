a4f93e23c0d0838c701b8b26ec56dab312fff6c3
Muhammad Raza Saeed <razasaeed135@gmail.com>
2025-02-04 17:01:41 +0500

    Added toast in FragmentB

--- app/src/main/java/com/practice/git/essentials/FragmentB.kt
+++ app/src/main/java/com/practice/git/essentials/FragmentB.kt
@@ -1,4 +1,12 @@
 package com.practice.git.essentials
 
-class FragmentB {
+import android.content.Context
+import android.widget.Toast
+
+class FragmentB(context: Context) {
+
+    init {
+        Toast.makeText(context, "Test Fragment B", Toast.LENGTH_SHORT).show()
+    }
+
 }
